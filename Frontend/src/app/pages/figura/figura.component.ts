398fca02db2b379ecae5d531d032f2dc60c158b5
mairimacnauh <mairim.acnauh@gmail.com>
2018-12-01 09:58:44 -0400

    report configuration

--- Frontend/src/app/pages/figura/figura.component.ts
+++ Frontend/src/app/pages/figura/figura.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AutosService} from "../../shared/services/autos.service";
+import {SensorService} from "../../shared/services/sensor.service";
 
 @Component({
   selector: 'app-figura',
@@ -7,15 +9,66 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FiguraComponent implements OnInit {
 
-  estados: any;
 
-  constructor() {
+  sensores: any;
+
+  avgConductividad: any;
+  avgPH: any;
+  avgTemperatura: any;
+  avgTurbidez: any;
+
+  currentConductividad: any;
+  currentPH: any;
+  currentTemperatura: any;
+  currentTurbidez: any;
+
+  avgDates: any;
+  currentDates: any;
+
+  constructor(private flujo: AutosService,
+              private sensor: SensorService,
+              private sensorService: SensorService) {
   }
 
   ngOnInit() {
+    // datos directos del sensor
+    this.sensorService.getAllSensores().subscribe(
+      res => this.sensores = res.body
+    );
+
+    this.avgDates = setInterval(() => {
+      this.sensorService.getAllSensoresAVG().subscribe(
+        res => {
+          this.avgConductividad = res.body[0].conductividad;
+          this.avgPH = res.body[0].ph;
+          this.avgTemperatura = res.body[0].temperatura;
+          this.avgTurbidez = res.body[0].turbidez;
+        }
+      );
+    }, 2000);
+
+    this.currentDates = setInterval(() => {
+      this.sensorService.getAllSensoresCurrent().subscribe(
+        res => {
+          this.currentConductividad = res.body[0].conductividad;
+          this.currentPH = res.body[0].ph;
+          this.currentTemperatura = res.body[0].temperatura;
+          this.currentTurbidez = res.body[0].turbidez;
+        }
+      );
+    }, 2000);
+
   }
 
-  addSemaforo(event) {
+  ngOnDestroy() {
+    clearInterval(this.avgDates);
+    clearInterval(this.currentDates);
+  }
+
+  getRegistroInfracciones(): void {
+    this.flujo.getRegistroInfracciones().subscribe(post => {
+      this.currentConductividad = post;
+    });
   }
 
 }
