ac725f630a56f2bef8b9890e56329df2a448f84a
mairimacnauh <mairim.acnauh@gmail.com>
2018-12-02 08:05:07 -0400

    registry configuration

--- Frontend/src/app/pages/datos-ph/datos-ph.component.ts
+++ Frontend/src/app/pages/datos-ph/datos-ph.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EstadosService} from '../../shared/services/estados.service';
+import {AutosService} from "../../shared/services/autos.service";
+import {SensorService} from "../../shared/services/sensor.service";
 
 @Component({
   selector: 'app-datos-ph',
@@ -8,64 +10,57 @@ import {EstadosService} from '../../shared/services/estados.service';
 })
 export class DatosPhComponent implements OnInit {
 
-  estados: any;
+  sensores: any;
 
-  settings = {
-    add: {
-      addButtonContent: '<i class="fa fa-chevron-up"></i>',
-      createButtonContent: '<i class="fa fa-chevron-up"></i>',
-      cancelButtonContent: '<i class="fa fa-chevron-up"></i>',
-    },
-    edit: {
-      editButtonContent: '<i class="fa fa-pencil-square fa-lg" aria-hidden="true" style="color:#2F4050">',
-      saveButtonContent: '<i class="fa fa-pencil-square fa-lg" aria-hidden="true" style="color:#2F4050">',
-      cancelButtonContent: '<i class="fa fa-pencil-square fa-lg" aria-hidden="true" style="color:#2F4050">',
-    },
-    delete: {
-      deleteButtonContent: '<i class="fa fa-trash fa-lg" aria-hidden="true" style="color:#2F4050"></i>',
-      confirmDelete: true
-    },
-    columns: {
-      id: {
-        title: 'ID',
-        filter: false
-      },
-      name: {
-        title: 'Full Name',
-        filter: false
-      },
-      username: {
-        title: 'User Name',
-        filter: false
-      },
-      email: {
-        title: 'Email',
-        filter: false
-      }
-    }
-  };
+  avgConductividad: any;
+  avgPH: any;
+  avgTemperatura: any;
+  avgTurbidez: any;
 
-  data = [
-    {id: 3, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 4, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 5, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 6, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 7, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 8, name: "riki", username: "sadas", email: "asdasd"},
-    {id: 3, name: "riki", username: "sadas", email: "asdasd"}
-  ];
+  currentConductividad: any;
+  currentPH: any;
+  currentTemperatura: any;
+  currentTurbidez: any;
 
+  avgDates: any;
+  currentDates: any;
 
-  constructor() {
-
+  constructor(private flujo: AutosService,
+              private sensor: SensorService,
+              private sensorService: SensorService) {
   }
 
   ngOnInit() {
 
+    this.sensorService.getAllSensores().subscribe(
+      res => this.sensores = res.body
+    );
+    this.avgDates = setInterval(() => {
+      this.sensorService.getAllSensoresAVG().subscribe(
+        res => {
+          this.avgConductividad = res.body[0].conductividad;
+          this.avgPH = res.body[0].ph;
+          this.avgTemperatura = res.body[0].temperatura;
+          this.avgTurbidez = res.body[0].turbidez;
+        }
+      );
+    }, 2000);
+
+    this.currentDates = setInterval(() => {
+      this.sensorService.getAllSensoresCurrent().subscribe(
+        res => {
+          this.currentConductividad = res.body[0].conductividad;
+          this.currentPH = res.body[0].ph;
+          this.currentTemperatura = res.body[0].temperatura;
+          this.currentTurbidez = res.body[0].turbidez;
+        }
+      );
+    }, 2000);
   }
 
-  onSearch(query: string = '') {
-
+  ngOnDestroy() {
+    clearInterval(this.avgDates);
+    clearInterval(this.currentDates);
   }
 }
 
